89e26ad7ae984bb9081a3dbb845c63e4dcac4b02
mansi2500 <m45808348@gmail.com>
2026-09-14 21:26:10 +0530

    Create binary_search.c

---
+++ binary_search.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, target, low, high, mid, index = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &target);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            index = mid;
+            break;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (index != -1) {
+        printf("Element found at index: %d\n", index);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
